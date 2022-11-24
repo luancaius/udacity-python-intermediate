19f243f03df480e58992c9ddd862ba58d6901411
luancaius <luan.ca@outlook.com>
2022-11-23 23:26:49 -0500

    changing model NearEarthObject

--- proj1/models.py
+++ proj1/models.py
@@ -44,10 +44,10 @@ class NearEarthObject:
         # You should coerce these values to their appropriate data type and
         # handle any edge cases, such as a empty name being represented by `None`
         # and a missing diameter being represented by `float('nan')`.
-        self.designation = ''
-        self.name = None
-        self.diameter = float('nan')
-        self.hazardous = False
+        self.designation = info['pdes']
+        self.name = info['name']
+        self.diameter = info['diameter']
+        self.hazardous = info['pha']
 
         # Create an empty initial collection of linked approaches.
         self.approaches = []
@@ -56,14 +56,14 @@ class NearEarthObject:
     def fullname(self):
         """Return a representation of the full name of this NEO."""
         # TODO: Use self.designation and self.name to build a fullname for this object.
-        return ''
+        return f'{self.designation}-{self.name}'
 
     def __str__(self):
         """Return `str(self)`."""
         # TODO: Use this object's attributes to return a human-readable string representation.
         # The project instructions include one possibility. Peek at the __repr__
         # method for examples of advanced string formatting.
-        return f"A NearEarthObject ..."
+        return f"A NearEarthObject named {self.name} ({self.designation}) with diameter {self.diameter} km of diameter"
 
     def __repr__(self):
         """Return `repr(self)`, a computer-readable string representation of this object."""
